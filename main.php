a3c335e2cba29c40b43cdd438ad3970894d7a5de
Antoine Poirier <poirierantoine52@gmail.com>
2023-12-24 15:55:48 +0100

    Article form (PT1.2)

--- main.php
+++ main.php
@@ -7,12 +7,47 @@
 	<link href="PapierDuStyle1.css" rel="stylesheet" type="text/css">
 </head>
 
+<?php 
+
+	function validation_article_saisi(){
+		// On vérifie d'abord si le titre et le contenu ont bien été saisis.
+		if (empty($_POST["articleTitre"]) or empty($_POST["articleContenu"])){
+			afficher_erreur("Titre et/ou contenu vide(s).");
+		} else {
+			// Dans le cas où les deux informations précédentes sont présentes, on
+			// vérifie si le contenu est d'une longueur autorisée.
+			$longueur = strlen($_POST["articleContenu"]);
+			if ( $longueur > 10000 ){
+				afficher_erreur("La longueur maximale est dépassée.");
+			} else {
+				// On vérifie si une image a été sélectionnée.
+				if (!empty($_FILES["articleImage"]["name"])){
+					afficher_remarque("Une image a été sélectionnée.");
+				} else {
+					afficher_remarque("Aucune image sélectionnée.");
+				}
+			}
+		}
+	}
+
+	function afficher_erreur(string $message){
+		echo("<div style='font-family:Arial Narrow;border-radius:4px;border:1px black solid;background-color:rgb(160,30,30);color:white;padding:3px;margin:3px;font-size:17px;width:30%;'><p style='margin:0 auto;'><b>ERREUR : </b>$message</p></div>");
+	}
+
+	function afficher_remarque(string $message){
+		echo("<div style='font-family:Arial Narrow;border-radius:4px;border:1px black solid;background-color:rgb(30,30,160);color:white;padding:3px;margin:3px;font-size:17px;width:30%;'><p style='margin:0 auto;'><b>INFO : </b>$message</p></div>");
+	}
+
+?>
+
 <body>
 
 	<main>
 
 	<!-- Formulaire de création pour concevoir un article -->
 	<form action="#" method="POST" class="creationArticle" enctype="multipart/form-data">
+
+		<?php validation_article_saisi(); ?>
 		
 		<!-- Insertion d'un titre pour l'article -->
 		<span for="titreArticle" class="creation_article_police"><b>Etape #1 :</b> Ecrire le titre de l'article :</span>
@@ -28,11 +63,6 @@
 
 		<input type="submit" value="Poster l'article" id="creationArticleBoutonPoste"/>
 	</form>
-
-	<?php 
-
-
-	?>
 
 	</main>
 
